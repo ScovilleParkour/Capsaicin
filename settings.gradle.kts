4234eb4b1b29df62cf08799e877c4d22304c7950
Melody <melodydevofficial@gmail.com>
2025-04-28 19:36:39 -0400

    Initial Commit

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,7 +10,7 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
 }
 
-rootProject.name = "fork"
+rootProject.name = "capsaicin"
 
-include("fork-api")
-include("fork-server")
+include("capsaicin-api")
+include("capsaicin-server")
